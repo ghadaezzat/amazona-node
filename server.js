6efab3726f0994049127ac68fdabd3816c80d023
ghadaezzat <ghadaezzat89@gmail.com>
2021-01-13 13:52:05 +0200

    added port

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.use(cors())
   res.json({user:"ghada ezzat"})   
  }
  )
-app.listen(config.port,(err)=>{console.log("magic happens on port awesome "+config.port)});
+app.listen((config.port || process.env.PORT),(err)=>{console.log("magic happens on port awesome "+config.port)});
 module.exports=app
