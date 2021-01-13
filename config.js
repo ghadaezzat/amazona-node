c520abb3994771d092f1c154814eff528a03289a
ghadaezzat <ghadaezzat89@gmail.com>
2021-01-13 14:03:19 +0200

    added port

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
+const PORT = process.env.PORT || 3000;
+
 module.exports={
   database:'mongodb+srv://ghada:amazona123@cluster0.xrn3z.mongodb.net/amazona?retryWrites=true&w=majority'  ,
-  port:3030,
-  secret:'ghadaezzatmoonlight789852'
+  port: PORT,
+  secret:'ghadaezzatmoonlight789852',
+  host:'0.0.0.0'
 }
