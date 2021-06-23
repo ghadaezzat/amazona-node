c828167461fb325f1e7692299e6dd022f113cf39
ghadaezzat <ghadaezzat89@gmail.com>
2021-06-24 01:45:56 +0200

    added mongolab database

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 3000;
 
 module.exports={
-  database:'mongodb+srv://ghada:amazona123@cluster0.xrn3z.mongodb.net/amazona?retryWrites=true&w=majority'  ,
+  database:'mongodb+srv://ghadaezzat:Ghada@220719@cluster0.xrn3z.mongodb.net/amazona?retryWrites=true&w=majority'  ,
   port: PORT,
   secret:'ghadaezzatmoonlight789852',
   host:'0.0.0.0'
